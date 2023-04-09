58fc7ab09442182c442feb1417cc253bddad8b6f
Kalle <kalle.paas@voco.ee>
2023-04-09 22:52:36 +0300

    Ex 2

--- resistorrun.ts
+++ resistorrun.ts
@@ -6,18 +6,12 @@ class Resistor {
     getCurrent(u: number): number {
         return u / this.r;
     }
+
+    getPower(u: number): number {
+        return u * this.getCurrent(u);
+    }
 }
 
 
-let r1: Resistor = new Resistor(110);
-let r2: Resistor = new Resistor(220);
-let r3: Resistor = new Resistor(4700);
-let resistors: Resistor[] = [];
-resistors.push(r1)
-resistors.push(r2)
-resistors.push(r3)
-let totalCurrent: number = 0;
-resistors.forEach((resistor) => {
-    totalCurrent += resistor.getCurrent(5)
-})
-console.log(totalCurrent);
+let r1 = new Resistor(220);
+console.log(r1.getPower(5));
